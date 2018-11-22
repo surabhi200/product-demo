5b6c41daa1945e3fd5b163db5ff304243725cc43
Kothari <surabhi.kothari@metronom-external.com>
2018-11-22 17:36:00 +0530

    commiting products files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//mport logo from './logo.svg';
+//import './App.css';
+import "./typo.less";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/*<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> 
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         
+    </header>*/}
+    <div id="app-root" class=" m">
+ 
+    <div class="m-page m-page-lightBlueBackground"></div>
+    <header class="m-header m-header-isFixed">
+    <div class="m-header-position">
+    <div class="m-header-inner">
+    <div class="m-header-logo"><img src="./brand-logo.png" alt="Logo" 
+    class="m-logo m-logo-isMakro m-logo-isSmall"></img>
+    <div class="m-header-content">
+    </div>
+    </div>
+    </div>
+    </div>
+  </header>
+
+<div class="m-page-container">
+</div>
+
+      <footer class="m-footer m-footer-isTransparent">
+      <span>
+        
+        <a href="#" class="m-footer-link">Terms &amp; Conditions</a>
+        </span><span><a href="#" class="m-footer-link">German</a>
+        <a href="#" class="m-footer-link m-footer-link-isActive">English</a>
+        <a href="#" class="m-footer-link">Français</a><a href="#" class="m-footer-link">Česky</a></span>
+        </footer>
       </div>
+      </div>
+
+    
     );
   }
 }
